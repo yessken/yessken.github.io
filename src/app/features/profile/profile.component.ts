28030b2304e36660e1f7ff9de5da0a4bca0b3b40
yessken <yessken@users.noreply.github.com>
2026-09-21 05:53:11 +0500

    fix: simplify mobile navigation

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -25,10 +25,11 @@ import { TelegramService } from '../../core/services/telegram.service';
         <p>Данные из Telegram (в боте будет отображаться ваш аккаунт).</p>
       }
       <nav>
-        <a routerLink="/" queryParamsHandling="preserve">Карта</a>
+        <a routerLink="/map" queryParamsHandling="preserve">Карта</a>
         <a routerLink="/events" queryParamsHandling="preserve">Сходки</a>
         <a routerLink="/my-tickets" queryParamsHandling="preserve">Мои билеты</a>
         <a routerLink="/create-event" queryParamsHandling="preserve">Создать событие</a>
+        <a routerLink="/tg-groups" queryParamsHandling="preserve">Сообщества</a>
       </nav>
     </div>
   `,
